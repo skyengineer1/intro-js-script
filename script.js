058ca37639872e28820bf91755931c9fced97d51
Sky <skyengineer001@gmail.com>
2025-03-10 13:01:28 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+let firstName = 'Goga';
+let lastName = 'Gabelia';
+let myJob = 'Software Developer';
+let myAge = 23;
+console.log(`My name is ${firstName} ${lastName}. I am ${myAge} years old ${myJob}`);
